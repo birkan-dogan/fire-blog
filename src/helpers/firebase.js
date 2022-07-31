1ad5a13ab2f6bf5c4ed197b7740531d34db36687
birkan <b.561birkan@gmail.com>
2022-07-31 21:23:46 +0300

    creating new user in firebase-authentication

---
+++ src/helpers/firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseUrl,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service. We will use this auth in our methods
+const auth = getAuth(app);
+
+// creating new user in firebase, and then we will call this function in Register page.
+export const createUser = async (email, password, navigate) => {
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
