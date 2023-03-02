3e3d02838782a0ebea67a88a7b807168390e6f8f
birkan <b.561birkan@gmail.com>
2023-03-02 22:52:34 +0300

    maintenance on Login component 🔐

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { AiOutlineGoogle, AiFillGithub } from "react-icons/ai";
-
 import { useNavigate } from "react-router-dom";
+import LoginComponent from "../components/login/LoginComponent";
 import {
   resetPassword,
   signIn,
   signUpProvider,
   signUpWithGithub,
 } from "../helpers/firebase";
+
 const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
@@ -25,73 +25,20 @@ const Login = () => {
   const forgotPassword = () => {
     resetPassword(email);
   };
+
+  const data = {
+    handleLogin,
+    setEmail,
+    setPassword,
+    forgotPassword,
+    navigate,
+    handleProvider,
+    handleGithub,
+  };
   return (
-    <div className="d-flex justify-content-center">
-      <div className="form-image d-none d-md-block">
-        <img src={"https://picsum.photos/800/800"} alt="fire-blog" />
-      </div>
-      <div className="register-form">
-        <h1 className="form-title display-3">Login</h1>
-        <form id="register" onSubmit={handleLogin}>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Enter Your Email"
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter Your Password"
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="d-flex">
-            <p className="link" onClick={forgotPassword}>
-              Forgot Password ?
-            </p>
-            <p className="link" onClick={() => navigate("/register")}>
-              Don't have an account?
-            </p>
-          </div>
-          <input
-            type="submit"
-            className="btn form-control text-white"
-            value="Login"
-          />
-        </form>
-        <div className="d-flex justify-content-center">
-          <button
-            className="btn text-white bg-danger "
-            onClick={handleProvider}
-          >
-            <AiOutlineGoogle
-              style={{ marginRight: "1rem", transform: "scale(1.7)" }}
-            />
-            Continue with Google
-          </button>
-          <button className="btn text-white bg-black " onClick={handleGithub}>
-            <AiFillGithub
-              style={{ marginRight: "1rem", transform: "scale(1.7)" }}
-            />
-            Continue with GitHub
-          </button>
-        </div>
-      </div>
-    </div>
+    <>
+      <LoginComponent {...data} />
+    </>
   );
 };
 
