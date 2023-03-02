1b1d68041dd79d23624d7bf21daf0d528742202f
birkan <b.561birkan@gmail.com>
2023-03-02 19:12:03 +0300

    maintenance on Register component 🧰

---
+++ src/components/register/RegisterComponent.jsx
@@ -0,0 +1,74 @@
+import { Link } from "react-router-dom";
+import img2 from "../../assets/rocket.svg";
+import { AiOutlineUser, AiOutlineLock, AiOutlineMail } from "react-icons/ai";
+import "../register/register.css";
+
+const RegisterComponent = ({
+  handleSubmit,
+  setFirstName,
+  setEmail,
+  setPassword,
+}) => {
+  return (
+    <div className="register">
+      <div className="form-image">
+        <img src={img2} alt="fire-blog" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title">Register</h1>
+        <form id="register" onSubmit={handleSubmit}>
+          <div className="input-box">
+            <label htmlFor="firstName" className="form-label">
+              <AiOutlineUser />
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="firstName"
+              placeholder="Enter Your First Name"
+              required
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="input-box">
+            <label htmlFor="email" className="form-label">
+              <AiOutlineMail />
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter Your Email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-box">
+            <label htmlFor="password" className="form-label">
+              <AiOutlineLock />
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter Your Password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="btn-container">
+            <input type="submit" className="btn" value="Register" />
+          </div>
+        </form>
+        <div className="one-of-us">
+          <h2>One of Us ?</h2>
+          <button>
+            <Link to={"/login"}>Log In</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterComponent;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,85 +2,28 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createUser } from "../helpers/firebase";
 
+import RegisterComponent from "../components/register/RegisterComponent";
+
 const Register = () => {
   const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const displayName = `${firstName} ${lastName}`;
+    const displayName = `${firstName}`;
     createUser(email, password, navigate, displayName);
   };
   return (
-    <div className="d-flex justify-content-center flex-wrap">
-      <div className="form-image d-none d-md-block">
-        <img src={"https://picsum.photos/800/800"} alt="fire-blog" />
-      </div>
-      <div className="register-form">
-        <h1 className="form-title display-3">Register</h1>
-        <form id="register" onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="firstName" className="form-label">
-              First Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="firstName"
-              placeholder="Enter Your First Name"
-              required
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="lastName" className="form-label">
-              Last Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="lastName"
-              placeholder="Enter Your Last Name"
-              required
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Enter Your Email"
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter Your Password"
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <input
-            type="submit"
-            className="btn form-control text-white"
-            value="Register"
-          />
-        </form>
-      </div>
-    </div>
+    <>
+      <RegisterComponent
+        handleSubmit={handleSubmit}
+        setFirstName={setFirstName}
+        setEmail={setEmail}
+        setPassword={setPassword}
+      />
+    </>
   );
 };
 
